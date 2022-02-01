7e40836c0734b08455408eebdb2459e0fcc2f189
Nikhil Das <91112508+nikhil-das-katonic@users.noreply.github.com>
2022-02-01 17:51:32 +0530

    Updates to app.py

--- app.py
+++ app.py
@@ -32,33 +32,28 @@ mysql = MySQL(app)
 
 @app.route('/home.html')
 def home():
-    return render_template('home.html')
+    if not session['loggedin']:
+        return render_template('home.html')
+    else:
+        return render_template('error.html')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-        # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
         account = cursor.fetchone()
-        # If account exists in accounts table in out database
         if account:
-            # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
-            # Redirect to home page
             return home()
         else:
-            # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
-        # Show the login form with message (if any)
     return render_template('login.html', msg=msg)
 
 
